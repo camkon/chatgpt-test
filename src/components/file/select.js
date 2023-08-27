e84282bbfad5a791910dbc73fbe68561b17395db
Ansaf Nisam <80558066+camkon@users.noreply.github.com>
2023-08-27 15:25:30 +0530

    file upload

--- src/components/file/select.js
+++ src/components/file/select.js
@@ -1,58 +1,76 @@
-import { Delete, Folder, Upload } from '@mui/icons-material'
-import { Box, Button, IconButton, Stack, TextField, Typography } from '@mui/material'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { api } from '../../core/api'
-import { useEffect, useState } from 'react'
-import InputFiles from 'react-input-files'
+import { Box, Button, Grid, IconButton, Typography } from '@mui/material'
+import { LoadingButton } from '@mui/lab'
+import { Delete, Folder, Upload } from '@mui/icons-material'
 
 const Select = ({selectedFiles, setSelectedFiles}) => {
 
-    const [file, setFile] = useState('')
+    const [uploading, setUploading] = useState(false)
 
     const handleUploadFile = async () => {
-        const formData = new FormData()
-        formData.append('uploadfiles', selectedFiles?.map(i => i))
-
-        console.log(selectedFiles?.map(i => i))
+        setUploading(true)
+        const formData = new FormData();
+        var fileIndex = 0
+        while(fileIndex < selectedFiles.length) {
+            formData.append('uploadfiles', selectedFiles[fileIndex]);
+            fileIndex++
+        }
 
         axios.post(api.docs_upload, formData, {headers: {'Content-Type': 'multipart/form-data'}
         }).then(res => {
-            setSelectedFiles([])
-            console.log(res?.data)
+            if(res?.data?.status === 200) {
+                setSelectedFiles([])
+            }
+            setUploading(false)
         }).catch(er => {
-            console.log(er)
+            setUploading(false)
         })
     }
 
-    // useEffect(() => {console.log(selectedFiles)}, [selectedFiles])
+    const handleSelectFiles = (e) => {
+        setSelectedFiles([...selectedFiles, ...e.target.files])
+    }
+
+    useEffect(() => {console.log(selectedFiles)}, [selectedFiles])
 
     return (
-        <Box sx={{borderBottom: '1px solid #2a2b32', }}>
-            <Box sx={{height: '8rem', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', columnGap: '0.5rem'}}>
-                <InputFiles multiple={true} onChange={(file) => {
-                    setFile(file)
-                    setSelectedFiles([...selectedFiles, file[0]])
-                }}>
-                    <Button variant='contained' sx={{background:'#202123', ':hover': {background: '#2a2b32'}}} startIcon={<Folder />}>Select Files</Button>
-                </InputFiles>
-                <Button onClick={handleUploadFile} variant='contained' sx={{background: '#202123', ':hover': {background: '#2a2b32'}}} startIcon={<Upload />}>Upload</Button>
-                {/* <IconButton onClick={handleUploadFile} sx={{padding: '1rem', borderRadius: '0.25rem', ':hover': {background: '#40414f'}, background: '#2a2b32', color: '#6b6c7b'}}><Upload /></IconButton> */}
+        <Box>
+            <Box sx={{padding: '2rem 0', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', columnGap: '0.5rem'}}>
+                <input 
+                    type="file"
+                    id="upload-files-button" 
+                    onChange={handleSelectFiles}
+                    hidden
+                    multiple
+                    accept='application/pdf'
+                />
+                <label htmlFor='upload-files-button'>
+                    <Button component='span' variant='contained' sx={{background:'#202123', ':hover': {background: '#2a2b32'}}} startIcon={<Folder />}>Select Files</Button>
+                </label>
             </Box>
-            {selectedFiles?.length && <Box sx={{height: 'auto', padding: '0.5rem', width: '30%', margin: '0 auto 2rem auto', borderRadius: '0.25rem', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', rowGap: '0.5rem', background: '#20212350', boxSizing: 'border-box'}}>
+
+            {selectedFiles?.length !== 0 && <Box sx={{height: 'auto', padding: '0.5rem', width: {xs: '90%', lg: '40%'}, margin: '0 auto 2rem auto', borderRadius: '0.25rem', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', rowGap: '0.5rem', background: '#20212350', boxSizing: 'border-box'}}>
                 {selectedFiles?.map((item, index) => {
-                    console.log(item)
                     return(
-                    <Stack width={'97%'} flexDirection='row' alignItems='center' columnGap={2} justifyContent='space-between' sx={{color: '#c9ccd3', padding: '0 0.5rem', borderRadius: '0.25rem', background: '#20212390', ':hover': {background: '#20212350'}}}>
-                        <Box sx={{display: 'flex', columnGap: '1rem'}}>
-                            <Typography>{index+1}</Typography>
-                            <Typography>{item.name}</Typography>
-                        </Box>
-                        <IconButton onClick={(e) => {
-                            setSelectedFiles(selectedFiles?.filter((i, ind) => ind !== index))
-                        }}><Delete sx={{color: '#c9ccd3'}}/></IconButton>
-                    </Stack>
+                    <Grid container sx={{justifyContent: 'space-between', alignItems: 'center', color: '#c9ccd3', padding: '0 0.5rem', margin: '0 0.5rem', borderRadius: '0.25rem', background: '#20212390', ':hover': {background: '#20212350'}, width: '100%'}}>
+                        <Grid item xs={0}><Typography>{index+1}</Typography></Grid>
+                        <Grid item xs={10}><Typography sx={{overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis'}}>{item.name}</Typography></Grid>
+                        
+                        <Grid item xs={0}>
+                            <IconButton sx={{borderRadius: '0.25rem'}} onClick={(e) => {
+                                setSelectedFiles(selectedFiles?.filter((i, ind) => ind !== index))
+                            }}><Delete sx={{color: '#c9ccd3'}}/></IconButton>
+                        </Grid>
+                    </Grid>
                 )})}
             </Box>}
+
+            {selectedFiles?.length !== 0 && <Box sx={{textAlign: 'center', paddingBottom: '2rem', borderBottom: '1px solid #2a2b32'}}>
+                <LoadingButton loading={uploading} onClick={handleUploadFile} variant='contained' sx={{color: '#fff', background: '#202123', ':hover': {background: '#2a2b32'}}} startIcon={<Upload />}>Upload Files</LoadingButton>    
+            </Box>}
+
         </Box>
     )
 }
--- src/layout.js
+++ src/layout.js
@@ -8,7 +8,7 @@ import File from './components/file'
 
 const Layout = () => {
 
-    const [page, setPage] = useState(true)
+    const [page, setPage] = useState(false)
 
     return (
         <Box sx={{height: '100vh', width: '100vw', background: '#343541'}}>
